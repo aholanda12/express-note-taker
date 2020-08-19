58a30da2f84b62b1ada66750dac01943c955e113
Ashley Holanda <aholanda12@gmail.com>
2020-08-19 17:06:07 -0400

    work done with tutor includes updating the routes and creating a server.js to connect to server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,64 +1,25 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+const path = require("path");
+const router = require("express").Router()
+const fs = require("fs")
+let notesData = [];
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+router.get("/notes", function(req, res){
+  // res.sendFile(path.join(__dirname, "./db/db.json"))
+notesData = fs.readFileSync("./db/db.json", "utf8")
+notesData = JSON.parse(notesData)
+res.json(notesData)
+})
 
+router.post("/notes", function(req, res){
+  notesData = fs.readFileSync("./db/db.json", "utf8")
+  notesData = JSON.parse(notesData)
+  req.body.id = notesData.length
+  notesData.push(req.body)
+  notesData = JSON.stringify(notesData)
+  fs.writeFile("./db/db.json", notesData, "utf8", function(err){
+    if(err) throw err
+  })
+  res.json(JSON.parse(notesData))
+})
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      // send a JSON string with the value true
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
-};
+module.exports = router
